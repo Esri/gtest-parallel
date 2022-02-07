efa627a5f927c0abb46effb144b026cdbb8c519f
Colin Powell <cpowell@esri.com>
2022-02-07 14:52:48 -0800

    script: fix bug in size filtering
    The --size argument needs to be passed to the gtest binary in addition
    to the subprocess that generates a list of tests to run. Failure to
    pass the size argument to the test binary may produce tests that _seem_
    to have run, but were not.

--- gtest_parallel.py
+++ gtest_parallel.py
@@ -757,14 +757,16 @@ def find_tests(binaries, additional_args, options, times):
     command = [test_binary] + additional_args
     if options.gtest_also_run_disabled_tests:
       command += ['--gtest_also_run_disabled_tests']
+    if options.size != '':
+      command += ['--size=' + options.size]
+      print('size filter: ON')
+
 
     list_command = command + ['--gtest_list_tests']
     if options.test != '':
       list_command += ['--test=' + options.test]
     if options.gtest_filter != '':
       list_command += ['--gtest_filter=' + options.gtest_filter]
-    if options.size != '':
-      list_command += ['--size=' + options.size]
 
     try:
       test_list = subprocess.check_output(list_command,
