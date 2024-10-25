814c965c659914d1b58ddb0114f59e7e3d2ae6c5
Kevin Parks <kparks@esri.com>
2024-10-25 11:54:07 -0700

    Remove now unnecessary escape sequences

--- gtest_parallel.py
+++ gtest_parallel.py
@@ -301,12 +301,12 @@ class XMLLogger(object):
     add useful information in this case
     """
     output = ""
-    start_pattern = re.compile(".*\[ *RUN *\].*" + test_name)
-    success_pattern = re.compile(".*\[ *OK *\].*" + test_name)
-    passed_pattern = re.compile("\[ *PASSED *\]")
-    failure_pattern = re.compile("\[ *FAILED *\]")
-    size_pattern = re.compile("\[ *SIZE *\]")
-    status_pattern = re.compile(".*\[(=*|-*)\].*") # brackets with = or - symbols
+    start_pattern = re.compile(".*[ *RUN *].*" + test_name)
+    success_pattern = re.compile(".*[ *OK *].*" + test_name)
+    passed_pattern = re.compile("[ *PASSED *]")
+    failure_pattern = re.compile("[ *FAILED *]")
+    size_pattern = re.compile("[ *SIZE *]")
+    status_pattern = re.compile(".*[(=*|-*)].*") # brackets with = or - symbols
     summary_pattern = re.compile("[0-9]+ FAILED TEST")
 
     with open(log_file) as log:
